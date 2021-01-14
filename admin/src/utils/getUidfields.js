9b6680807737d985785e703fa503072d945b1e68
boazpoolman <info@boazpoolman.nl>
2021-01-14 22:47:09 +0100

    Fix type error in getUidFields helper function

--- admin/src/utils/getUidfields.js
+++ admin/src/utils/getUidfields.js
@@ -1,7 +1,7 @@
 export const getUidFieldsByContentType = (contentType) => {
   let uidFieldNames = [];
   
-  Object.entries(contentType.schema.attributes).map(([i, e]) => {
+  Object.entries(contentType.attributes).map(([i, e]) => {
     if (e.type === "uid") {
       uidFieldNames.push(i);
     }
