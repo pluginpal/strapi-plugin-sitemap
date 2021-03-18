98810768fe56d71935eb70b9c4349cafca3e71c8
Boaz Poolman <boaz_skater@hotmail.com>
2021-03-18 22:02:53 +0100

    WIP: rewrite ConfigPage container from class component to functional component

--- admin/src/containers/ConfigPage/index.js
+++ admin/src/containers/ConfigPage/index.js
@@ -1,75 +1,36 @@
-/*
- *
- * ConfigPage
- *
- */
-
-import React, { Component } from 'react';
-import pluginId from '../../pluginId';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useGlobalContext, HeaderNav } from 'strapi-helper-plugin';
 import { isEmpty } from 'lodash';
-
-import { HeaderNav } from 'strapi-helper-plugin';
-import Header from '../../components/Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faPlus,
-} from '@fortawesome/free-solid-svg-icons';
-import List from '../../components/List';
 import { Button } from '@buffetjs/core';
+import { Map } from 'immutable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+import { deleteContentType, discardModifiedContentTypes, getContentTypes, getSettings, hasSitemap, onChangeContentTypes, onChangeSettings, populateSettings, submit, submitModal } from '../../state/actions/Sitemap';
+import pluginId from '../../helpers/pluginId';
+import Header from '../../components/Header';
+import List from '../../components/List';
 import ModalForm from '../../components/ModalForm';
-import { submit, getSettings, populateSettings, getContentTypes, onChangeContentTypes, submitModal, onChangeSettings, deleteContentType, generateSitemap, discardAllChanges, discardModifiedContentTypes, hasSitemap } from './actions';
-import { bindActionCreators, compose } from 'redux';
-import { connect } from 'react-redux';
-import selectConfigPage from './selectors';
-import reducer from './reducer';
-import saga from './saga';
 import SettingsForm from '../../components/SettingsForm';
 import Wrapper from '../../components/Wrapper';
-import { GlobalContext } from 'strapi-helper-plugin'
 import { ContainerFluid } from './components';
 
-class ConfigPage extends Component {
-  static contextType = GlobalContext;
+const ConfigPage = (props) => {
+  const { formatMessage } = useGlobalContext();
+  const [settingsType, setSettingsType] = useState('');
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.get('sitemap'), Map());
 
-  headerNavLinks = [
-    {
-      name: 'Collection entries',
-      to: `/plugins/${pluginId}/collection-entries`,
-    },
-    {
-      name: 'Custom entries',
-      to: `/plugins/${pluginId}/custom-entries`,
-    },
-  ];
+  useEffect(() => {
+    dispatch(getSettings());
+    dispatch(getContentTypes());
+    dispatch(hasSitemap());
+    setSettingsType(getSettingsType());
+  }, []);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      settingsType: ''
-    }
-  }
-  
-  componentDidMount() {
-    this.props.getSettings();
-    this.props.hasSitemap();
-    this.props.getContentTypes();
-    this.setState({ 'settingsType': this.getSettingsType()});
-  }
-
-  componentDidUpdate(prevProps) {
-    // Get new settings on navigation change
-    if (prevProps.match.params.env !== this.props.match.params.env) {
-      this.props.getSettings();
-    }
-
-    if (prevProps.match.path !== this.props.match.path) {
-      this.setState({ 'settingsType': this.getSettingsType()});
-    }
-  }
-
-  getSettingsType() {
-    const settingsUrl = this.props.match.path.split("/").pop();
+  const getSettingsType = () => {
+    const settingsUrl = props.match.path.split("/").pop();
     const settingsType =
       settingsUrl === 'collection-entries' ? 'Collection' :
       settingsUrl === 'custom-entries' && 'Custom';
@@ -77,125 +38,93 @@ class ConfigPage extends Component {
     return settingsType;
   }
 
-  handleModalSubmit(e) {
+  const handleModalSubmit = (e) => {
     e.preventDefault();
-    return this.props.submitModal();
+    dispatch(submitModal());
   }
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    return this.props.submit();
+    dispatch(submit());
   }
 
-  render() {
-    if (isEmpty(this.props.contentTypes)) {
-      return (<div />);
-    }
+  console.log(state.toJS());
 
-    return (
-      <div>
-        <ContainerFluid>
-          <Header 
-            onSubmit={(e) => this.handleSubmit(e)}
-            onCancel={(e) => this.props.discardAllChanges()}
-            settings={this.props.settings}
-            initialData={this.props.initialData}
-            generateSitemap={async () => {
-              await this.props.generateSitemap();
-              this.props.hasSitemap();
-            }}
-            sitemapPresence={this.props.sitemapPresence}
-            hasSitemap={this.props.hasSitemap}
-          />
-          <HeaderNav
-            links={this.headerNavLinks}
-            style={{ marginTop: '4.6rem' }}
-          />
-          <List 
-            settingsType={this.state.settingsType}
-            settings={this.props.settings}
-            onDelete={this.props.deleteContentType}
-          />
-          <Wrapper style={{ paddingTop: 0, paddingBottom: 0, marginBottom: 0 }}>
-            <Button 
-              color="primary" 
-              icon={<FontAwesomeIcon icon={faPlus} />} 
-              label={this.context.formatMessage({ id: 'sitemap.Button.AddAll' })}
-              onClick={() => this.props.populateSettings()}
-              hidden={this.state.settingsType === 'Custom' || !isEmpty(this.props.settings.contentTypes)}
-            />
-            <Button 
-              color="primary" 
-              icon={<FontAwesomeIcon icon={faPlus} />} 
-              label={this.context.formatMessage({ id: 'sitemap.Button.AddURL' })}
-              onClick={() => this.props.history.push({ search: 'addNew' })}
-              hidden={this.state.settingsType === 'Collection' || !isEmpty(this.props.settings.customEntries)}
-            />
-            <Button 
-              color="secondary"
-              style={{ marginLeft: 15 }}
-              icon={<FontAwesomeIcon icon={faPlus} />} 
-              label={this.context.formatMessage({ id: 'sitemap.Button.Add1by1' })}
-              onClick={() => this.props.history.push({ search: 'addNew' })}
-              hidden={this.state.settingsType === 'Custom' || !isEmpty(this.props.settings.contentTypes)}
-            />
-          </Wrapper>
-          <ModalForm 
-            contentTypes={this.props.contentTypes}
-            modifiedContentTypes={this.props.modifiedContentTypes}
-            modifiedCustomEntries={this.props.modifiedCustomEntries}
-            settings={this.props.settings}
-            settingsType={this.state.settingsType}
-            onSubmit={(e) => this.handleModalSubmit(e)}
-            onCancel={this.props.discardModifiedContentTypes}
-            onChange={this.props.onChangeContentTypes}
-          />
-          <SettingsForm 
-            onChange={this.props.onChangeSettings} 
-            settings={this.props.settings}
-          />
-        </ContainerFluid>
-      </div>
-    );
-  }
-};
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      getSettings,
-      getContentTypes,
-      deleteContentType,
-      discardAllChanges,
-      discardModifiedContentTypes,
-      onChangeContentTypes,
-      onChangeSettings,
-      submit,
-      populateSettings,
-      submitModal,
-      generateSitemap,
-      hasSitemap
-    },
-    dispatch
+  // if (isEmpty(dispatch(contentTypes)) ){
+  //   return (<div />);
+  // }
+
+  return (
+    <div>
+      <ContainerFluid>
+        <Header 
+          onSubmit={(e) => handleSubmit(e)}
+          onCancel={() => dispatch(discardAllChanges())}
+          settings={state.get('settings')}
+          initialData={state.get('initialData')}
+          generateSitemap={() => dispatch(generateSitemap())}
+          sitemapPresence={state.get('sitemapPresence')}
+        />
+        <HeaderNav
+          links={[
+            {
+              name: 'Collection entries',
+              to: `/plugins/${pluginId}/collection-entries`,
+            },
+            {
+              name: 'Custom entries',
+              to: `/plugins/${pluginId}/custom-entries`,
+            },
+          ]}
+          style={{ marginTop: '4.6rem' }}
+        />
+        <List 
+          settingsType={settingsType}
+          settings={state.get('settings')}
+          onDelete={dispatch(deleteContentType())}
+        />
+        <Wrapper style={{ paddingTop: 0, paddingBottom: 0, marginBottom: 0 }}>
+          <Button 
+            color="primary" 
+            icon={<FontAwesomeIcon icon={faPlus} />} 
+            label={formatMessage({ id: 'sitemap.Button.AddAll' })}
+            onClick={() => dispatch(populateSettings())}
+            hidden={settingsType === 'Custom' || !isEmpty(state.getIn(['settings', 'contentTypes']))}
+          />
+          <Button 
+            color="primary" 
+            icon={<FontAwesomeIcon icon={faPlus} />} 
+            label={formatMessage({ id: 'sitemap.Button.AddURL' })}
+            onClick={() => props.history.push({ search: 'addNew' })}
+            hidden={settingsType === 'Collection' || !isEmpty(state.getIn(['settings', 'customEntries']))}
+          />
+          <Button 
+            color="secondary"
+            style={{ marginLeft: 15 }}
+            icon={<FontAwesomeIcon icon={faPlus} />} 
+            label={formatMessage({ id: 'sitemap.Button.Add1by1' })}
+            onClick={() => props.history.push({ search: 'addNew' })}
+            hidden={settingsType === 'Custom' || !isEmpty(state.getIn(['settings', 'contentTypes']))}
+          />
+        </Wrapper>
+        <ModalForm 
+          contentTypes={state.get('contentTypes')}
+          modifiedContentTypes={state.get('modifiedContentTypes')}
+          modifiedCustomEntries={state.get('modifiedCustomEntries')}
+          settings={state.get('settings')}
+          settingsType={settingsType}
+          onSubmit={(e) => handleModalSubmit(e)}
+          onCancel={dispatch(discardModifiedContentTypes())}
+          onChange={dispatch(onChangeContentTypes())}
+        />
+        <SettingsForm 
+          onChange={dispatch(onChangeSettings())} 
+          settings={state.get('settings')}
+        />
+      </ContainerFluid>
+    </div>
   );
 }
-
-const mapStateToProps = selectConfigPage();
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps
-);
-
-const withReducer = strapi.injectReducer({
-  key: 'configPage',
-  reducer,
-  pluginId,
-});
-const withSaga = strapi.injectSaga({ key: 'configPage', saga, pluginId });
-
-export default compose(
-  withReducer,
-  withSaga,
-  withConnect
-)(ConfigPage);
+ 
+export default ConfigPage;
