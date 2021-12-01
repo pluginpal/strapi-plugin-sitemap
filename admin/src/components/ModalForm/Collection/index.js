126ae3907a7cde58bcb2d2fd3e0e2ba0f61202cc
Boaz Poolman <boaz_skater@hotmail.com>
2021-12-01 22:19:40 +0100

    feat: Pattern field autocomplete

--- admin/src/components/ModalForm/Collection/index.js
+++ admin/src/components/ModalForm/Collection/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { useIntl } from 'react-intl';
 import { isEmpty } from 'lodash/fp';
+import styled from 'styled-components';
 
 import { Grid, GridItem } from '@strapi/design-system/Grid';
 import { TextInput } from '@strapi/design-system/TextInput';
 import { Select, Option } from '@strapi/design-system/Select';
+import { Popover } from '@strapi/design-system/Popover';
+import { Box } from '@strapi/design-system/Box';
+import { Stack } from '@strapi/design-system/Stack';
 
 import SelectContentTypes from '../../SelectContentTypes';
 
 import form from '../mapper';
 import SelectLanguage from '../../SelectLanguage';
+import useActiveElement from '../../../helpers/useActiveElement';
 
 const CollectionForm = (props) => {
   const { formatMessage } = useIntl();
+  const activeElement = useActiveElement();
+  const [showPopover, setShowPopover] = useState(false);
+  const patternRef = useRef();
 
   const {
     contentTypes,
@@ -36,6 +44,17 @@ const CollectionForm = (props) => {
     onCancel(false);
   };
 
+  useEffect(() => {
+    if (
+      modifiedState.getIn([uid, 'languages', langcode, 'pattern'], '').endsWith('[')
+      && activeElement.name === 'pattern'
+    ) {
+      setShowPopover(true);
+    } else {
+      setShowPopover(false);
+    }
+  }, [modifiedState.getIn([uid, 'languages', langcode, 'pattern'], ''), activeElement]);
+
   const patternHint = () => {
     const base = 'Create a dynamic URL pattern';
     let suffix = '';
@@ -54,6 +73,13 @@ const CollectionForm = (props) => {
 
     return base + suffix;
   };
+
+  const HoverBox = styled(Box)`
+    cursor: pointer;
+    &:hover:not([aria-disabled='true']) {
+      background: ${({ theme }) => theme.colors.primary100};
+    }
+  `;
 
   return (
     <form style={{ borderTop: '1px solid #f5f5f6', paddingTop: 30 }}>
@@ -81,21 +107,47 @@ const CollectionForm = (props) => {
         <GridItem col={6} s={12}>
           <Grid gap={4}>
             <GridItem col={12}>
-              <TextInput
-                label={formatMessage({ id: 'sitemap.Settings.Field.Pattern.Label' })}
-                name="pattern"
-                value={modifiedState.getIn([uid, 'languages', langcode, 'pattern'], '')}
-                hint={patternHint()}
-                disabled={!uid || (contentTypes[uid].locales && !langcode)}
-                error={patternInvalid.invalid ? patternInvalid.message : ''}
-                placeholder="/en/pages/[id]"
-                onChange={async (e) => {
-                  if (e.target.value.match(/^[A-Za-z0-9-_.~[\]/]*$/)) {
-                    onChange(uid, langcode, 'pattern', e.target.value);
-                    setPatternInvalid({ invalid: false });
-                  }
-                }}
-              />
+              <div ref={patternRef}>
+                <TextInput
+                  label={formatMessage({ id: 'sitemap.Settings.Field.Pattern.Label' })}
+                  name="pattern"
+                  value={modifiedState.getIn([uid, 'languages', langcode, 'pattern'], '')}
+                  hint={patternHint()}
+                  disabled={!uid || (contentTypes[uid].locales && !langcode)}
+                  error={patternInvalid.invalid ? patternInvalid.message : ''}
+                  placeholder="/en/pages/[id]"
+                  onChange={async (e) => {
+                    if (e.target.value.match(/^[A-Za-z0-9-_.~[\]/]*$/)) {
+                      onChange(uid, langcode, 'pattern', e.target.value);
+                      setPatternInvalid({ invalid: false });
+                    }
+                  }}
+                />
+              </div>
+              {(patternRef && showPopover) && (
+                <Popover
+                  source={patternRef}
+                  spacing={-14}
+                  fullWidth
+                >
+                  <Stack size={1}>
+                    {allowedFields[uid].map((fieldName) => (
+                      <HoverBox
+                        key={fieldName}
+                        padding={2}
+                        onClick={() => {
+                          console.log('set in pattern', fieldName);
+                          const newPattern = `${modifiedState.getIn([uid, 'languages', langcode, 'pattern'], '')}${fieldName}]`;
+
+                          onChange(uid, langcode, 'pattern', newPattern);
+                        }}
+                      >
+                        {fieldName}
+                      </HoverBox>
+                    ))}
+                  </Stack>
+                </Popover>
+              )}
             </GridItem>
             {Object.keys(form).map((input) => (
               <GridItem col={12} key={input}>
