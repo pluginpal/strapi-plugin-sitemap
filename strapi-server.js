76c5eb9ed2559d4c972e7d910ac07454c58d0774
Boaz Poolman <boaz_skater@hotmail.com>
2021-12-01 13:28:58 +0100

    feat: Use sitemap_exclude field instead of putting excluded id's in core_store

--- strapi-server.js
+++ strapi-server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const bootstrap = require('./server/bootstrap');
+const register = require('./server/register');
 const services = require('./server/services');
 const routes = require('./server/routes');
 const config = require('./server/config');
@@ -9,6 +10,7 @@ const controllers = require('./server/controllers');
 module.exports = () => {
   return {
     bootstrap,
+    register,
     routes,
     config,
     controllers,
