429b9849acba71948b282ef23ee098e131eb90ac
Boaz Poolman <boaz_skater@hotmail.com>
2021-12-08 13:25:46 +0100

    fix: Use parameters also in count query

--- server/utils/index.js
+++ server/utils/index.js
@@ -12,7 +12,7 @@ const logMessage = (msg = '') => `[strapi-plugin-sitemap]: ${msg}`;
 
 const noLimit = async (query, parameters, limit = 100) => {
   let entries = [];
-  const amountOfEntries = await query.count();
+  const amountOfEntries = await query.count(parameters);
 
   for (let i = 0; i < (amountOfEntries / limit); i++) {
     /* eslint-disable-next-line */
