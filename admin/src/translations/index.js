b0e993c6f47ab63a917fa18a4136103eceed5c2b
yasudacloud <hello@yasuda.cloud>
2022-03-12 21:06:23 +0900

    Japanese translation added

--- admin/src/translations/index.js
+++ admin/src/translations/index.js
@@ -4,6 +4,7 @@ import en from './en.json';
 import es from './es.json';
 import fr from './fr.json';
 import it from './it.json';
+import ja from './ja.json';
 import ko from './ko.json';
 import nl from './nl.json';
 import pl from './pl.json';
@@ -22,6 +23,7 @@ const trads = {
   es,
   fr,
   it,
+  ja,
   ko,
   nl,
   pl,
