8cdef25575943ba8a0e5a0e8218f613cecac8c01
Boaz Poolman <boaz_skater@hotmail.com>
2023-06-20 18:51:57 +0200

    feat: Grant permissions for all public endpoints of the plugin to the public role

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -6,6 +6,33 @@ module.exports = async () => {
   const sitemap = strapi.plugin('sitemap');
 
   try {
+    // Give the public role permissions to access the public API endpoints.
+    if (strapi.plugin('users-permissions')) {
+      const roles = await strapi
+        .service("plugin::users-permissions.role")
+        .find();
+
+      const _public = await strapi
+        .service("plugin::users-permissions.role")
+        .findOne(roles.filter((role) => role.type === "public")[0].id);
+
+      _public.permissions['plugin::sitemap'] = {
+        controllers: {
+          core: {
+            getSitemap: { enabled: true },
+            getSitemapXsl: { enabled: true },
+            getSitemapXslCss: { enabled: true },
+            getSitemapXslJs: { enabled: true },
+            getSitemapXslSortable: { enabled: true },
+          },
+        },
+      };
+
+      await strapi
+        .service("plugin::users-permissions.role")
+        .updateRole(_public.id, _public);
+    }
+
     // Load lifecycle methods for auto generation of sitemap.
     await sitemap.service('lifecycle').loadAllLifecycleMethods();
 
@@ -15,12 +42,6 @@ module.exports = async () => {
         section: 'plugins',
         displayName: 'Access the plugin settings',
         uid: 'settings.read',
-        pluginName: 'sitemap',
-      },
-      {
-        section: 'plugins',
-        displayName: 'Menu link to plugin settings',
-        uid: 'menu-link',
         pluginName: 'sitemap',
       },
     ];
