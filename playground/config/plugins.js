81f51865fb4cbb3f8a30c22a4f3e52cf62f89338
Boaz Poolman <boaz_skater@hotmail.com>
2023-07-10 08:03:13 +0200

    fix: Import resolve path to plugin in playground

--- playground/config/plugins.js
+++ playground/config/plugins.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
   sitemap: {
     enabled: true,
-    resolve: path.resolve(__dirname, '../../../src/plugins/sitemap'),
+    resolve: path.resolve(__dirname, '../src/plugins/sitemap'),
   },
 };
