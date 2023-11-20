3cf5049d2e692552995be6e0666d4db5aa71d24e
Göksal Çoşar <goksal.cosar@enuygun.com>
2023-11-20 11:15:55 +0300

    all language filter closed

--- admin/src/components/SelectLanguage/index.jsx
+++ admin/src/components/SelectLanguage/index.jsx
@@ -22,26 +22,26 @@ const SelectLanguage = (props) => {
         hint={formatMessage({ id: 'sitemap.Settings.Field.SelectLanguage.Description', defaultMessage: 'Select a language.' })}
         onChange={(newValue) => onChange(newValue)}
         value={value}
-        disabled={value === 'und'}
+        // disabled={value === 'und'}
       >
         {Object.entries(contentType.locales).map(([uid, displayName]) => {
           return <Option value={uid} key={uid}>{displayName}</Option>;
         })}
       </Select>
-      <Box paddingTop={4} paddingBottom={4}>
-        <Checkbox
-          onValueChange={(cbValue) => {
-            if (value === 'und') {
-              onChange(null);
-            } else {
-              onChange('und');
-            }
-          }}
-          value={value === 'und'}
-        >
-          {formatMessage({ id: 'sitemap.Settings.Field.SelectLanguage.SameForAll', defaultMessage: 'Same for all languages' })}
-        </Checkbox>
-      </Box>
+      {/*<Box paddingTop={4} paddingBottom={4}>*/}
+      {/*  <Checkbox*/}
+      {/*    onValueChange={(cbValue) => {*/}
+      {/*      if (value === 'und') {*/}
+      {/*        onChange(null);*/}
+      {/*      } else {*/}
+      {/*        onChange('und');*/}
+      {/*      }*/}
+      {/*    }}*/}
+      {/*    value={value === 'und'}*/}
+      {/*  >*/}
+      {/*    {formatMessage({ id: 'sitemap.Settings.Field.SelectLanguage.SameForAll', defaultMessage: 'Same for all languages' })}*/}
+      {/*  </Checkbox>*/}
+      {/*</Box>*/}
     </div>
   );
 };
