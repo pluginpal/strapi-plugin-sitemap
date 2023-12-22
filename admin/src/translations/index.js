b517115d6a7b3a821530e2c2c6b6b6f7bd7b8856
HighError <hierrorua@gmail.com>
2023-12-22 11:09:38 +0200

    refactor: 💡 Returned single singleQuotes

--- admin/src/translations/index.js
+++ admin/src/translations/index.js
@@ -1,21 +1,21 @@
-import ar from "./ar.json";
-import de from "./de.json";
-import en from "./en.json";
-import es from "./es.json";
-import fr from "./fr.json";
-import it from "./it.json";
-import ja from "./ja.json";
-import ko from "./ko.json";
-import nl from "./nl.json";
-import pl from "./pl.json";
-import ptBR from "./pt-BR.json";
-import pt from "./pt.json";
-import ru from "./ru.json";
-import tr from "./tr.json";
-import uk from "./uk.json";
-import vi from "./vi.json";
-import zhHans from "./zh-Hans.json";
-import zh from "./zh.json";
+import ar from './ar.json';
+import de from './de.json';
+import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import it from './it.json';
+import ja from './ja.json';
+import ko from './ko.json';
+import nl from './nl.json';
+import pl from './pl.json';
+import ptBR from './pt-BR.json';
+import pt from './pt.json';
+import ru from './ru.json';
+import tr from './tr.json';
+import uk from './uk.json';
+import vi from './vi.json';
+import zhHans from './zh-Hans.json';
+import zh from './zh.json';
 
 const trads = {
   ar,
@@ -28,13 +28,13 @@ const trads = {
   ko,
   nl,
   pl,
-  "pt-BR": ptBR,
+  'pt-BR': ptBR,
   pt,
   ru,
   tr,
   uk,
   vi,
-  "zh-Hans": zhHans,
+  'zh-Hans': zhHans,
   zh,
 };
 
